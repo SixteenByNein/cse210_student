b2fcdbaf2007ad1e257c226d5b2ed9d9b6b17a5c
SixteenByNein <gunnarlorenzoyoung@gmail.com>
2025-01-24 00:40:54 -0700

    Completed assignment

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -6,16 +6,28 @@ class Program
     static void Main(string[] args)
     {
 
+        Job job0 = new Job();
+
+        job0._company = "Porsche";
+        job0._jobTitle = "Graphic Designer";
+        job0.startYear = 2013;
+        job0._endYear = 2020;
+
         Job job1 = new Job();
-        job1._company = "Google";
-        job1._jobTitle = "supervisor";
 
-        Job job2 = new Job();
-        job2._company = "Valve";
-        job2._jobTitle = "Artist";
+        job1._company = "LEGO";
+        job1._jobTitle = "Master Builder";
+        job1.startYear = 2020;
+        job1._endYear = 2025;
 
+        Resume resume1 = new Resume();
+        resume1._name = "Gunnar Young";
+        
+        resume1._jobs.Add(job0);
+        resume1._jobs.Add(job1);
 
-        job1.Display();
+        resume1.Display();
+
 
     }
 }
