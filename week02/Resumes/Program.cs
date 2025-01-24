49519ba48e65920bf8e72dc827af82d791d108b6
SixteenByNein <gunnarlorenzoyoung@gmail.com>
2025-01-24 00:16:52 -0700

    Steps 1 - 5

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "supervisor";
+
+        Job job2 = new Job();
+        job2._company = "Valve";
+        job2._jobTitle = "Artist";
+
+
+        job1.Display();
+
     }
 }
